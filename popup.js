3c30a3badb91725e4d1157dc4bcf773f08f4ee08
Nutan22341 <nutan22341@iiitd.ac.in>
2024-09-14 16:27:47 +0530

    changes done

--- popup.js
+++ popup.js
@@ -1,14 +1,28 @@
-let btn = document.getElementById("clickButton");
+let btn = document.getElementById("summarize");
 btn.addEventListener("click", (event) => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  chrome.tabs.query({ active: true, currentWindow: true },  (tabs) => {
     if (tabs.length === 0) {
       console.error("No active tab found.");
       return;
     }
     console.log(tabs)
-    chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id },
-      files: ["content.js"],
-    });
+    tab_url = tabs[0].url
+    console.log(tabs[0].url)
+    async function fetch_data() {
+      const url = `https://extractorapi.com/api/v1/extractor/?apikey=de070375909a4b85d4aaf1e2d8b5f36328adaef9&url=${tab_url}`
+      try{
+        const response = await fetch(url)
+        if(!response.ok){
+          console.log("nhi ho paa rha h ")
+          throw new Error(response.status)
+        }
+        const json_format = await response.json()
+        console.log(json_format)
+      }
+      catch(e){
+        console.log(e)
+      }
+    }
+    fetch_data()
   });
 });
