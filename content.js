9c0070b4c0c604a140fc37fe41f73fa33d881b31
Nikhil <nikhil22322@iiitd.ac.in>
2024-09-14 14:47:30 +0530

    popup nd content

--- content.js
+++ content.js
@@ -1,10 +1,5 @@
-
-
-console.log("erfgtyh")
-console.log("erfgtyh")
-console.log("erfgtyh")
-console.log("erfgtyh")
-console.log("erfgtyh")
-console.log("erfgtyh")
-console.log("erfgtyh")
-console.log("erfgtyh")
+//importScripts('Readbility.js');
+let doc = document.cloneNode(true);
+let reader = new Readability(doc);
+let article = reader.parse();
+console.log(article)
